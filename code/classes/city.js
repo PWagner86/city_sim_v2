9b38112e11bed7a6e78e9e3a0b86b1cbfa87aa73
Peter Wagner <peter.wagner1986@gmail.com>
2024-07-23 17:38:57 +0200

    (feature): added first street

--- code/classes/city.js
+++ code/classes/city.js
@@ -11,5 +11,16 @@ export default class City {
     init(ctx) {
         this.matrix.setMatrix((this.width / 50), (this.height / 30));
         this.matrix.draw(ctx);
+        console.log(this.matrix.tiles.find(t=>t.id === 14));
+        this.#drawStreets(ctx);
+    }
+
+    #drawStreets(ctx) {
+        ctx.fillStyle = "red";
+        for(const tile of this.matrix.tiles) {
+            if(tile.y === 260 && (tile.x >= 0 || tile.x <= 980)) ctx.fillRect(tile.x, tile.y, tile.width, tile.height);
+            if(tile.y === 280 && (tile.x >= 0 || tile.x <= 980)) ctx.fillRect(tile.x, tile.y, tile.width, tile.height);
+            
+        }
     }
 }
--- code/classes/primitives/tile.js
+++ code/classes/primitives/tile.js
@@ -22,6 +22,10 @@ export default class Tile {
         if(fill) {
             ctx.fillStyle = color;
             ctx.fill();
-        }
+        };
+
+        ctx.strokeText(this.id, this.center.x, this.center.y);
+        ctx.textAlign = "center";
+        ctx.baseLine = "middle";
     }
 }
